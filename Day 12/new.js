263e19bda2028709a35fd377620243f1c49824ae
DINESH D <d.dinesh8489@gmail.com>
2025-08-06 10:20:35 +0530

    add basic of JavaScript

---
+++ Day 12/new.js
@@ -0,0 +1,23 @@
+let s=10;
+let z="10";
+let h="Hello";
+let r="world";
+let e=true;
+let f=null;
+let g=undefined;
+if (s===z){
+    console.log("s and z are equal in value");
+    }
+else {
+    console.log("s and z are not equal in value");
+}
+if (f===g){
+    console.log("f and g are equal in value");
+    }   
+else {
+    console.log("f and g are not equal in value");
+}
+console.log("s is of type: " + typeof s);
+console.log("z is of type: " + typeof z);
+console.log(s+z);
+console.log(h+r);
---
+++ Day 12/new1.js
@@ -0,0 +1,7 @@
+let a=[1,2,3,4,5];
+let b={ name: "John", age: 30, city: "New York" };
+console.log(b.name +" is "+b.age+" years old and lives in "+b.city+".");
+
+for (let i=0; i<a.length; i++){
+    console.log(a[i]);
+}
